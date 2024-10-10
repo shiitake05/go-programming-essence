ebfa94fd697ceece59b146e1580e42bed859d7ba
shiitake05 <pegtar377512@gmail.com>
2024-10-10 13:16:24 +0900

    initial commit

---
+++ EmbedDirectory/go.mod
@@ -0,0 +1,3 @@
+module EmbedDirectory
+
+go 1.23.1
---
+++ EmbedDirectory/main.go
@@ -0,0 +1,34 @@
+// staticというディレクトリに置かれたファイルがembed-logo.phgとして生成される
+package main
+
+import (
+	"embed"
+	"io"
+	"log"
+	"os"
+	"path"
+)
+
+//go:embed static
+var local embed.FS
+
+func main() {
+	fis, err := local.ReadDir("static")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range fis {
+		in, err := local.Open(path.Join("static", fi.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		out, err := os.Create("embed-" + path.Base(fi.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		io.Copy(out, in)
+		out.Close()
+		in.Close()
+		log.Println("exported", "embed-"+path.Base(fi.Name()))
+	}
+}
