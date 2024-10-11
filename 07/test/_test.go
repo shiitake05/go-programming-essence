e2d8bf08a53e9eba328e60e3a92a3242e89a7c1d
shiitake05 <pegtar377512@gmail.com>
2024-10-11 21:15:01 +0900

    initial commit

---
+++ 07/test/_test.go
@@ -0,0 +1,15 @@
+// テストコード
+package hsd
+
+import (
+	"testing"
+)
+
+func TestStringDistance(t *testing.T) {
+	got := StringDistance("foo", "foh")
+	// want := 2
+	want := 1
+	if got != want {
+		t.Fatalf("expected: %v, got %v:", want, got)
+	}
+}
---
+++ 07/test/main.go
@@ -0,0 +1,19 @@
+// テスト対象のコード
+package hsd
+
+func StringDistance(lhs, rhs string) int {
+	return Distance([]rune(lhs), []rune(rhs))
+}
+
+func Distance(a, b []rune) int {
+	dist := 0
+	if len(a) != len(b) {
+		return -1
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			dist++
+		}
+	}
+	return dist
+}
