16bb41994544dcbc18f8668646b6c1e2e990188d
shiitake05 <pegtar377512@gmail.com>
2024-10-11 19:07:07 +0900

    initial commit

---
+++ 06/goroutine/main.c
@@ -0,0 +1,39 @@
+#define _OPEN_THREADS
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *thread(void *arg) {
+  pthread_t self = pthread_self();
+  int i = 0;
+  for (i = 0; i < 10000; i++) {
+    flockfile(stdout);
+    printf("%ld\n", self);
+    fflush(stdout);
+    funlockfile(stdout);
+  }
+  pthread_exit(0);
+}
+
+int main() {
+  pthread_t thid[100];
+  int i;
+  void *ret;
+
+  for (i = 0; i < 100; i++) {
+    if (pthread_create(&thid[i], NULL, thread, NULL) != 0) {
+      perror("pthread_create() error");
+      exit(1);
+    }
+  }
+
+  for (i = 0; i < 100; i++) {
+    if (pthread_join(thid[i], NULL) != 0) {
+      perror("pthread_join() error");
+      exit(1);
+    }
+  }
+}
+
+// CよりGoのほうが早い
---
+++ 06/goroutine/main.go
@@ -0,0 +1,35 @@
+// 「go build -o goroutine-test main.go」の後、「./goroutine-test」で実行
+// 「ps a -o pid,nlwp,args | grep goroutine-test | grep -v grep」で確認
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// func doSomething(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	time.Sleep(100 * time.Second)
+// }
+
+// 負荷が高くなるようにする
+func doSomething(wg *sync.WaitGroup, id int) {
+	defer wg.Done()
+	// time.Sleep(100 * time.Second)
+	for i := 0; i < 10000; i++ {
+		fmt.Printf("%d\n", id)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		// go doSomething(&wg)
+		go doSomething(&wg, i)
+	}
+	wg.Wait()
+}
+
+#cgo CFLAGS: -I. // これを追加
