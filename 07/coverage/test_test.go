df65b2425624deb23eb922e336c751d843149f90
shiitake05 <pegtar377512@gmail.com>
2024-10-12 13:31:09 +0900

    initial commit

---
+++ 07/coverage/test_test.go
@@ -0,0 +1,20 @@
+// テストコード
+package hsd
+
+import (
+	"testing"
+)
+
+func TestStringDistance(t *testing.T) {
+	got := StringDistance("foo", "foh")
+	want := 1
+	if got != want {
+		t.Fatalf("expected: %v, got %v:", want, got)
+	}
+
+	got = StringDistance("foo", "foobar")
+	want = -1
+	if got != want {
+		t.Fatalf("expected: %v, got %v:", want, got)
+	}
+}
