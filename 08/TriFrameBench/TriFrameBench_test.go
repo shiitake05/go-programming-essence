450f7e2d0072e0ad771e944762313e9f10eb5b83
shiitake05 <pegtar377512@gmail.com>
2024-10-13 12:50:18 +0900

    initial commit

---
+++ 08/TriFrameBench/TriFrameBench_test.go
@@ -0,0 +1,72 @@
+package TriFrameBench_test
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"testing"
+
+	"github.com/go-gota/gota/dataframe"
+	"github.com/rocketlaunchr/dataframe-go/experts"
+	"github.com/rocketlaunchr/dataframe-go/imports"
+	"github.com/tobgu/qframe"
+)
+
+func BenchmarkQFrame(b *testing.B) {
+	b ,err := ioutil.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		qf := qframe.ReadCSV(bytes.NewReader(bs))
+		qf = qf.Select("sepal_length", "species")
+		err = qf.ToCSV(%out)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkGota(b *testing.B) {
+	bs, err := ioutil.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		df := dataframe.ReadCSV(bytes.NewReader(bs))
+		df = df.Select([]string{"sepal_length", "species"})
+		err = df.WriteCSV(&out, dataframe.WriteHeader(true))
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkDataframeGo(b *testing.B) {
+	bs, err := ioutil.ReadFile("iris.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var out bytes.Buffer
+		df, err := imports.LoadFromCSV(context.Background(), bytes.NewReader(bs))
+		if err != nil {
+			b.Fatal(err)
+		}
+		df.RemoveSeries("sepal_width")
+		df.RemoveSeries("petal_length")
+		df.RemoveSeries("petal_width")
+		err = experts.ExportToCSV(context.Background(), &out, df)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
---
+++ 08/TriFrameBench/go.mod
@@ -0,0 +1,18 @@
+module TriFrameBench
+
+go 1.23.1
+
+require (
+	github.com/go-gota/gota v0.12.0 // indirect
+	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/guptarohit/asciigraph v0.5.1 // indirect
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mauricelam/genny v0.0.0-20190320071652-0800202903e5 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	github.com/rocketlaunchr/dataframe-go v0.0.0-20211025052708-a1030444159b // indirect
+	github.com/tobgu/qframe v0.4.0 // indirect
+	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
+)
