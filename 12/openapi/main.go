0a60846ccdd7390e23690b7440400c111e5e2e45
shiitake05 <pegtar377512@gmail.com>
2024-10-14 17:11:01 +0900

    initial commit

---
+++ 12/openapi/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+
+	"openapi/ent"
+	"openapi/ent/todo"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+)
+
+type Api struct {
+	client *ent.Client
+}
+
+type ListTodoParams struct {
+	Page         *int
+	ItemsPerPage *int
+}
+
+func (a *Api) ListTodo(ctx echo.Context, params ListTodoParams) error {
+	page := 0
+	if params.Page != nil {
+		page = *params.Page
+	}
+	itemsPerPage := 5
+	if params.ItemsPerPage != nil {
+		itemsPerPage = *params.ItemsPerPage
+	}
+	ees, err := a.client.Todo.Query().
+		Order(ent.Desc(todo.FieldID)).
+		Offset(page * itemsPerPage).
+		Limit(itemsPerPage).
+		All(context.Background())
+	if err != nil {
+		log.Println(err)
+		return echo.ErrBadRequest
+	}
+	return ctx.JSON(200, ees)
+}
+
+func (a *Api) CreateTodo(ctx echo.Context) error {
+	var ee ent.Todo
+	err := json.NewDecoder(ctx.Request().Body).Decode(&ee)
+	if err != nil {
+		log.Println(err)
+		return echo.ErrBadRequest
+	}
+	e := a.client.Todo.Create().
+		SetText(ee.Text).
+		SetStatus(ee.Status).
+		SetPriority(ee.Priority)
+	if !ee.CreatedAt.IsZero() {
+		e.SetCreatedAt(ee.CreatedAt)
+	}
+	if ee2, err := e.Save(context.Background()); err != nil {
+		log.Println(err)
+		return echo.ErrBadRequest
+	} else {
+		ee = *ee2
+	}
+	return ctx.JSON(200, ee)
+}
+
+func (a *Api) DeleteTodo(ctx echo.Context, id int) error {
+	e := a.client.Todo.DeleteOneID(id)
+	err := e.Exec(context.Background())
+	if err != nil {
+		log.Println(err)
+		return echo.ErrBadRequest
+	}
+	return nil
+}
+
+func (a *Api) ReadTodo(ctx echo.Context, id int) error {
+	e, err := a.client.Todo.Get(context.Background(), int(id))
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return echo.ErrNotFound
+		}
+		log.Println(err)
+		return echo.ErrBadRequest
+	}
+	return ctx.JSON(200, e)
+}
+
+func (a *Api) UpdateTodo(ctx echo.Context, id int) error {
+	var ee ent.Todo
+	err := json.NewDecoder(ctx.Request().Body).Decode(&ee)
+	if err != nil {
+		log.Println(err)
+		return echo.ErrBadRequest
+	}
+	e := a.client.Todo.UpdateOneID(int(id)).
+		SetText(ee.Text).
+		SetStatus(ee.Status).
+		SetPriority(ee.Priority)
+	if ee2, err := e.Save(context.Background()); err != nil {
+		log.Println(err)
+		return echo.ErrBadRequest
+	} else {
+		ee = *ee2
+	}
+	return ctx.JSON(200, ee)
+}
+
+func main() {
+	client, err := ent.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+
+	client.Schema.Create(context.Background())
+
+	e := echo.New()
+	myApi := &Api{client: client}
+	RegisterHandlers(e, myApi)
+	e.Static("/", "static")
+	e.Logger.Fatal(e.Start(":8989"))
+}
